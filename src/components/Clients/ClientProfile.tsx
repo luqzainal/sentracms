9314caba3e51b468dd09a09f3df886bc9de59710
EvoDagang <evodagang.malaysia@gmail.com>
2025-07-12 13:31:10 +0800

    Wrap JSX elements in React Fragment

--- src/components/Clients/ClientProfile.tsx
+++ src/components/Clients/ClientProfile.tsx
@@ -138,6 +138,7 @@ const ClientProfile: React.FC<ClientProfileProps> = ({ clientId, onBack, onEdit 
     }
   };
   return (
+    <>
     <div className="p-6 space-y-6">
       {/* Header */}
       <div className="flex items-center justify-between">
@@ -450,6 +451,7 @@ const ClientProfile: React.FC<ClientProfileProps> = ({ clientId, onBack, onEdit 
         />
       )}
     </div>
+    </>
   );
 };
 
